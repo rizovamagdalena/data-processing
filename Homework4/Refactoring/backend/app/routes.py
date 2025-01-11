1731bf7400da958f513f697cb4256f838593d01e
Dimitrij Jovanovski <150369784+dimitrijovanovski@users.noreply.github.com>
2025-01-11 23:22:31 +0100

    Update routes.py

--- Homework4/Refactoring/backend/app/routes.py
+++ Homework4/Refactoring/backend/app/routes.py
@@ -1,4 +1,5 @@
 import sqlite3
+from datetime import datetime, timedelta
 
 from flask import Blueprint, jsonify, request
 from src.backend.repositories.stock_repository import StockRepository
@@ -17,11 +18,28 @@ def get_all_stocks():
 @stock_routes.route('/api/stocks/<string:code>', methods=['GET'])
 def get_stock_data(code):
     try:
-        start_date = request.args.get('start_date')
-        end_date = request.args.get('end_date')
-        stock_data = StockRepository.fetch_stock_data_by_code(code, start_date, end_date)
+        # Get 'start_date' and 'end_date' from query parameters
+        start_date_str = request.args.get('start_date')
+        end_date_str = request.args.get('end_date')
+
+        # Default to the last day if not provided
+        if not start_date_str:
+            start_date = datetime.now() - timedelta(days=1)  # Default to 1 day ago
+        else:
+            start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
+
+        if not end_date_str:
+            end_date = datetime.now()  # Default to current date
+        else:
+            end_date = datetime.strptime(end_date_str, "%Y-%m-%d")
+
+        print(f"start: {start_date}, end: {end_date}")  # Debugging output
+
+        # Fetch the stock data for the specified period
+        stock_data = StockRepository.search_stock_data_by_code_in_interval(code, start_date, end_date)
         if not stock_data:
             return jsonify({"message": f"No data found for stock code {code}."}), 404
+
         return jsonify({"data": stock_data}), 200
     except sqlite3.DatabaseError as e:
         return jsonify({"error": "Database error", "message": str(e)}), 500
