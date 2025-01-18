39bc694e19d36ca0bedb16dde8ff7c5b2fc0eb17
Dimitrij Jovanovski <150369784+dimitrijovanovski@users.noreply.github.com>
2025-01-18 19:30:21 +0100

    Update main.py

--- Homework4/Refactoring/main.py
+++ Homework4/Refactoring/main.py
@@ -1,9 +1,11 @@
 # main.py
-from src.backend.app import app
+from src.backend.app.app import app
 from src.backend.app.app import scheduler
 from src.backend.services.stock_service import StockService
 from apscheduler.triggers.interval import IntervalTrigger
 from datetime import datetime
+
+from src.technical_analysis.TechnicalAnalysis import TechnicalAnalysis
 
 if __name__ == "__main__":
     # Schedule the scraping task
@@ -12,7 +14,15 @@ if __name__ == "__main__":
         IntervalTrigger(hours=24),
         next_run_time=datetime.now()
     )
+
+    scheduler.add_job(
+        TechnicalAnalysis.init,
+        IntervalTrigger(hours=24),
+        next_run_time=datetime.now()
+    )
+
     scheduler.start()
+
 
     # Run the Flask app
     app.run(debug=True)
