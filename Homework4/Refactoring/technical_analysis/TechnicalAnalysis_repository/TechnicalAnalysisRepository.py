315242745b8b9186b729ff645354113fb3bd60da
Dimitrij Jovanovski <150369784+dimitrijovanovski@users.noreply.github.com>
2025-01-18 19:29:06 +0100

    Create TechnicalAnalysisRepository.py

---
+++ Homework4/Refactoring/technical_analysis/TechnicalAnalysis_repository/TechnicalAnalysisRepository.py
@@ -0,0 +1,61 @@
+import os
+from datetime import datetime, timedelta
+import pandas as pd
+from tornado.http1connection import parse_int
+
+from src.technical_analysis.TechnicalAnalysis_models.database import Database
+
+
+class TechnicalAnalysisRepository:
+
+    @staticmethod
+    def initialize_database():
+        db = Database()
+        db.execute('''CREATE TABLE IF NOT EXISTS stock_indicators (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        code TEXT,
+        date TEXT,
+        time_period TEXT,
+        cci REAL,
+        vpt REAL,
+        signal TEXT
+    );''')
+        db.commit()
+
+
+    @staticmethod
+    def insert_row(row, code, date_str, time_period):
+        db = Database()
+
+        existing = db.execute("""
+                    SELECT 1 FROM stock_indicators 
+                    WHERE code = ? AND date = ? AND time_period = ?""", (code, date_str, time_period))
+
+        if not existing:
+            db.execute("""
+                        INSERT INTO stock_indicators (code, date, time_period, cci, vpt, signal)
+                        VALUES (?, ?, ?, ?, ?, ?)""",
+                         (code, date_str, time_period, row['CCI'], row['VPT'], row['Signal']))
+
+        db.commit()
+
+    @staticmethod
+    def fetch_stock_indicators_for_code_in_period(stock_code, period):
+        db = Database()
+        query = "SELECT * FROM stock_indicators WHERE code LIKE ?"
+        query += " AND time_period BETWEEN ? AND ?"
+
+        start_date = ''
+        end_date = datetime.now().strftime('%Y-%m-%d')
+
+        if period=='1D':
+            start_date = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
+
+        if period=='1W':
+            start_date = (datetime.now() - timedelta(weeks=1)).strftime('%Y-%m-%d')
+
+        if period=='1M':
+            start_date = (datetime.now() - timedelta(weeks=4)).strftime('%Y-%m-%d')
+
+        db.execute(query, (f"{stock_code}%", start_date, end_date))
+        return db.fetchall()
