a38f90389974c58304ab41d27319ab8b303f3851
Dimitrij Jovanovski <150369784+dimitrijovanovski@users.noreply.github.com>
2025-01-18 21:36:00 +0100

    Update main.py

--- Homework4/Devision into MicroServices/StockScraper/main.py
+++ Homework4/Devision into MicroServices/StockScraper/main.py
@@ -1,14 +1,6 @@
 from flask import Flask, request, jsonify
 from src.stock_service import StockService
 
-'''import requests
-
-test_data = {"records": [{"code": "TEST", "date": "01.01.2023", "last_price": 123.45, "max_price": 150.0, "min_price": 120.0, "avg_price": 135.0, "percent_change": "5%", "quantity": 100, "revenue_best_denars": 12345.67, "total_revenue_denars": 56789.01}]}
-
-response = requests.post("http://127.0.0.1:5003/api/stocks", json=test_data)
-print(response.status_code)
-print(response.json())'''
-
 app = Flask(__name__)
 
 @app.route('/scrape', methods=['GET', 'POST'])
